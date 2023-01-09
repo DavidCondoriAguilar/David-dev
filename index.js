f353d33582d1098b6c7bc53eca8296698747b194
DavidWilld <103283145+DavidWilld@users.noreply.github.com>
2023-01-09 12:54:52 -0500

    first try

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+
+var typed = new Typed('.type', {
+    strings: ['Designer&#127912;','Developer&#128187;','Freelancer&#x1F3F9;'],
+    typeSpeed:150,
+    backSpeed:150,
+    loop: true
+});
+
+

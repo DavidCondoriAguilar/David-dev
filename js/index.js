8871340aa5610ea98af0526ebc3f137db58f719f
DavidWilld <103283145+DavidWilld@users.noreply.github.com>
2023-06-03 15:49:41 -0500

    do
    news-tecnology's

--- js/index.js
+++ js/index.js
@@ -1,13 +1,37 @@
-
-
-
-
-var typed = new Typed('.type', {
-    strings: ['Developer&#128187;','Designer&#128736;','Freelancer&#10166;'],
-    typeSpeed:150,
-    backSpeed:150,
-    loop: true
+let typed = new Typed(".type", {
+  strings: ["Developer&#128187;", "Designer&#128736;", "Freelancer&#10166;"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
 });
 
-var sound = new Audio();
-sound.src="mario-bros tuberia.mp3"
+
+let sound = new Audio();
+sound.src = "mario-bros tuberia.mp3";
+
+
+// const icon = document.getElementById("links");
+// icon.addEventListener("mouseover", () => {
+//   icon.style.transform = "translateY(212px)";
+// });
+// icon.addEventListener("mouseout", () => {
+//   icon.style.transform = "none";
+// });
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const icon = document.getElementById('my-icon');
+  
+//     if (icon) {
+//       icon.addEventListener('mouseover', () => {
+//         icon.style.transform = 'translateY(32px)';
+//       });
+  
+//       icon.addEventListener('mouseout', () => {
+//         icon.style.transform = 'none';
+//       });
+//     } else {
+//       console.log('Elemento no encontrado');
+//     }
+//   });
+  
